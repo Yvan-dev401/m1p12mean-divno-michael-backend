b991a84efc6a176506475debd2c275bd66bc0280
Divno Yvan <97512903+Yvan-dev401@users.noreply.github.com>
2025-03-29 22:46:48 +0300

    Ajout de la gestion des commandes et mise à jour de la route de sortie pour enregistrer les sorties de stock et terminer les réparations

--- routes/commande.js
+++ routes/commande.js
@@ -4,18 +4,26 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
     try {
-        const { stockId, quantite, date, nomPiece} = req.body;
-        const result = await req.db.collection('stocks').insertOne(
-            {
-                stockId: new ObjectId(stockId),
-                quantite,
-                date: date || new Date(),
-                nomPiece
-            }
-        );
-        res.status(201).json(result.ops[0]);
+        const { idStock, orderQuantite, date, nomPiece, etat } = req.body;
+        const result = await req.db.collection("commande").insertOne({
+          idStock: idStock,
+          orderQuantite,
+          date: date || new Date(),
+          nomPiece,
+          etat: etat || "En attente",
+        });
+        res.status(201).json({ message: "Commande avec succès", result });
     } catch (error) {
         res.status(400).json({ message: error.message });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const commandes = await req.db.collection('commande').find().toArray();
+        res.json(commandes);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
