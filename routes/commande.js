b930249d39bd2c6c71d402c7b79b56797fb059d2
Divno Yvan <97512903+Yvan-dev401@users.noreply.github.com>
2025-03-30 09:51:23 +0300

    Correction de la configuration de la base de données et amélioration de la gestion des commandes

--- routes/commande.js
+++ routes/commande.js
@@ -4,21 +4,6 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
     try {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        const { stockId, quantite, date, nomPiece} = req.body;
-        const result = await req.db.collection('commande').insertOne(
-            {
-                stockId: new ObjectId(stockId),
-                quantite,
-                date: date || new Date(),
-                nomPiece
-            }
-        );
-        res.status(201).json(result.ops[0]);
-=======
-=======
->>>>>>> e9b7168bfe81c4f3a21e97b6f3f4da4b6b398c7a
         const { idStock, orderQuantite, date, nomPiece, etat } = req.body;
         const result = await req.db.collection("commande").insertOne({
           idStock: idStock,
@@ -28,10 +13,6 @@ router.post('/', async (req, res) => {
           etat: etat || "En attente",
         });
         res.status(201).json({ message: "Commande avec succès", result });
-<<<<<<< HEAD
->>>>>>> e9b7168bfe81c4f3a21e97b6f3f4da4b6b398c7a
-=======
->>>>>>> e9b7168bfe81c4f3a21e97b6f3f4da4b6b398c7a
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
