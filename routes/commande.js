2216a868cf84b7419e65465613243a6cbdb86127
michael78r <michaelrakotonanahary78@gmail.com>
2025-03-30 13:24:54 +0300

    Merge branch 'main' of https://github.com/Yvan-dev401/m1p12mean-divno-michael-backend

--- routes/commande.js
+++ routes/commande.js
@@ -4,21 +4,6 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
     try {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        const { stockId, quantite, date, nomPiece} = req.body;
-        const result = await req.db.collection('commande').insertOne(
-            {
-                stockId: new ObjectId(stockId),
-                quantite,
-                date: date || new Date(),
-                nomPiece
-            }
-        );
-        res.status(201).json(result.ops[0]);
-=======
-=======
->>>>>>> e9b7168bfe81c4f3a21e97b6f3f4da4b6b398c7a
         const { idStock, orderQuantite, date, nomPiece, etat } = req.body;
         const result = await req.db.collection("commande").insertOne({
           idStock: idStock,
@@ -28,10 +13,6 @@ router.post('/', async (req, res) => {
           etat: etat || "En attente",
         });
         res.status(201).json({ message: "Commande avec succès", result });
-<<<<<<< HEAD
->>>>>>> e9b7168bfe81c4f3a21e97b6f3f4da4b6b398c7a
-=======
->>>>>>> e9b7168bfe81c4f3a21e97b6f3f4da4b6b398c7a
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
@@ -46,4 +27,43 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    try {
+        const commandeId = req.params.id;
+        const { etat } = req.body;
+
+        if (!etat) {
+            return res.status(400).json({ message: 'Le champ "etat" est requis' });
+        }
+
+        const commande = await req.db.collection('commande').findOne({ _id: new ObjectId(commandeId) });
+
+        if (!commande) {
+            return res.status(404).json({ message: 'Commande non trouvée' });
+        }
+
+        const result = await req.db.collection('commande').updateOne(
+            { _id: new ObjectId(commandeId) },
+            { $set: { etat } }
+        );
+
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ message: 'Commande non trouvée' });
+        }
+
+        // Si l'état est modifié en "Livré", ajouter la quantité commandée à la quantité disponible dans la collection stocks
+        if (etat === 'Livré') { // Mise à jour uniquement si l'état est "Livré"
+            const { idStock, orderQuantite } = commande;
+
+            await req.db.collection('stocks').updateOne(
+                { _id: new ObjectId(idStock) },
+                { $inc: { quantiteDisponible: orderQuantite } } // Ajouter la quantité commandée à la quantité disponible
+            );
+        }
+
+        res.json({ message: 'État de la commande mis à jour avec succès' });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
 module.exports = router;
