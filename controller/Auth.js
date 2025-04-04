c73366ea78f7e60d91298681f240f82ef12f40bf
Divno Yvan <97512903+Yvan-dev401@users.noreply.github.com>
2025-04-04 06:45:26 +0300

    Suppression de fichiers inutilisés et remplacement de bcrypt par bcryptjs

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 
 async function Log(req, res) {
     const { username } = req.body;
--- routes/utilisateurRoute.js
+++ routes/utilisateurRoute.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {Log, Logout} = require('../controller/Auth');
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 // const express = require('express');
 
 router.post('/login', Log);
