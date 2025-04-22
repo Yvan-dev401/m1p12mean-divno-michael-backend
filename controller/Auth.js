a712156730cc1353d4b0af01d6cce333ffe7dca8
michael78r <michaelrakotonanahary78@gmail.com>
2025-04-22 22:57:36 +0300

    delete cookie

--- controller/Auth.js
+++ controller/Auth.js
@@ -32,14 +32,14 @@ async function Log(req, res) {
         // };
 
         const token = generateAccessJWT(user); // Assurez-vous d'avoir une fonction pour générer le JWT
-        res.cookie("SessionID", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: 'none',
-            //domain: 'm1p12mean-divno-michael-backend.onrender.com',
-            path: '/',
-            maxAge: 86400000    
-        })
+        // res.cookie("SessionID", token, {
+        //     httpOnly: true,
+        //     secure: true,
+        //     sameSite: 'none',
+        //     //domain: 'm1p12mean-divno-michael-backend.onrender.com',
+        //     path: '/',
+        //     maxAge: 86400000    
+        // })
 
         res.status(200).json({
             status: "success",
--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.get('/test', (req, res) => {
-  res.json("mety amzay");
+  res.json("mbola ndray");
 });
 app.use('/probleme', require('./routes/problemeRoute'));
 app.use('/devis', require('./routes/devisRoute'));
